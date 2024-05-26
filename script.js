54a362c759d7bb6e3ceb07289bf72eb758529203
Akshay <103642286+Akshay1495@users.noreply.github.com>
2024-05-26 21:01:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+
+document.querySelector('input[type="button"][value="Select and Remove"]').addEventListener('click', function() {
+    const colorSelect = document.getElementById('colorSelect');
+    colorSelect.remove(colorSelect.selectedIndex);
+});
+
